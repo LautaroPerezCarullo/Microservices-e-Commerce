4bdcb5c3260a1055fe27c2198215d057ad4aa9c3
LautaroPerezCarullo <laupercar1100@gmail.com>
2024-12-01 20:32:34 -0300

    Add cache to stockMS

--- catalog_service/app/service.py
+++ catalog_service/app/service.py
@@ -10,7 +10,7 @@ class ProductService:
     def save(self, product: Product) -> Product:
         #Save new product
         saved_product = repository.save(product)
-        cache.set(f'producto_{saved_product.id}', saved_product, timeout=15)
+        cache.set(f'product_{saved_product.id}', saved_product, timeout=120)
         return saved_product
 
     def all(self) -> List[Product]:
@@ -18,18 +18,15 @@ class ProductService:
         products = cache.get('all_products')
         if not products:
             products = repository.all()
-            if products: cache.set('all_products', products, timeout=3600) 
+            if products: cache.set('all_products', products, timeout=120) 
         return products
 
     def find(self, id: int) -> Optional[Product]:
-        print("Producto obtenido del cache")
         #Return product by id
-        product = cache.get(f'producto_{id}')
+        product = cache.get(f'product_{id}')
         if product is None:
             product = repository.find(id)
-            if product: cache.set(f'producto_{id}', product, timeout=3600)
-        else:
-            print("Producto obtenido del cache")
+            if product: cache.set(f'product_{id}', product, timeout=120)
         return product
 
     # def save(self, product: Product) -> Product:
--- stock_service/app/service.py
+++ stock_service/app/service.py
@@ -1,5 +1,6 @@
 from .model import Stock
 from .repository import StockRepository
+from app import cache
 from .exceptions import InsufficientStockError
 
 repository = StockRepository()
@@ -8,9 +9,21 @@ class StockService:
 
     def save(self, stock: Stock) -> Stock:
         #Save new stock
-        if stock.input_output == 2 and stock.amount > self.calculate_stock(stock):
-            raise InsufficientStockError(f"Insufficient Stock, Product {stock.product_id}")
+        self.existing_stock = cache.get(f"existing_stock_product_{stock.product_id}")
+        if self.existing_stock is None:
+            self.existing_stock = self.calculate_stock(stock)
+            cache.set(f"existing_stock_product_{stock.product_id}", self.existing_stock, timeout = 120)
+    
+        if stock.input_output == 2:
+            if stock.amount > self.existing_stock:
+                raise InsufficientStockError(f"Insufficient Stock, Product {stock.product_id}")
+            else:
+                self.existing_stock = self.existing_stock - stock.amount
+                cache.set(f"existing_stock_product_{stock.product_id}", self.existing_stock, timeout = 120)
+                return repository.save(stock)
         else:
+            self.existing_stock = self.existing_stock + stock.amount
+            cache.set(f"existing_stock_product_{stock.product_id}", self.existing_stock, timeout = 120)
             return repository.save(stock)
     
     def calculate_stock(self, stock: Stock) -> int:
