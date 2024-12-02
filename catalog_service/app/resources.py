89d47943a1f56dbbc9e91febccd9983d21d8dff0
LautaroPerezCarullo <laupercar1100@gmail.com>
2024-12-02 04:30:25 -0300

    Add retry to catalog MS

--- catalog_service/app/resources.py
+++ catalog_service/app/resources.py
@@ -12,20 +12,30 @@ product_service = ProductService()
 @product_bp.route('/products', methods=['GET'])
 def view_catalog():
     response_builder = ResponseBuilder()
-    data = product_schema.dump(product_service.all(), many=True)
-    response_builder.add_message("Finded Products").add_status_code(200).add_data(data)
-    return response_schema.dump(response_builder.build()), 200
+    try:
+        data = product_schema.dump(product_service.all(), many=True)
+        response_builder.add_message("Finded Products").add_status_code(200).add_data(data)
+        return response_schema.dump(response_builder.build()), 200
+    
+    except Exception as e:
+        response_builder.add_message(f"Something happended getting catalog: {str(e)}").add_status_code(500)
+        return response_schema.dump(response_builder.build()), 500
 
 @product_bp.route('/products/<int:id>', methods=['GET'])
 def find_product(id):
     response_builder = ResponseBuilder()
     data = None
-    product = product_service.find(id)
-    if product:
-        data = product_schema.dump(product)
-    if data:
-        response_builder.add_message("Finded Product").add_status_code(200).add_data(data)
-        return response_schema.dump(response_builder.build()), 200
-    else:
-        response_builder.add_message("Any Product Finded").add_status_code(404)
-        return response_schema.dump(response_builder.build()), 404
+    try:
+        product = product_service.find(id)
+        if product:
+            data = product_schema.dump(product)
+        if data:
+            response_builder.add_message("Finded Product").add_status_code(200).add_data(data)
+            return response_schema.dump(response_builder.build()), 200
+        else:
+            response_builder.add_message("Any Product Finded").add_status_code(404)
+            return response_schema.dump(response_builder.build()), 404
+    
+    except Exception as e:
+        response_builder.add_message(f"Something happended getting product({id}): {str(e)}").add_status_code(500)
+        return response_schema.dump(response_builder.build()), 500
--- catalog_service/app/service.py
+++ catalog_service/app/service.py
@@ -1,33 +1,48 @@
 from .model import Product
 from .repository import ProductRepository
 from app import cache
+from tenacity import retry, wait_fixed, stop_after_attempt
 from typing import List, Optional
+from sqlalchemy.exc import OperationalError
+import logging
+
 
 repository = ProductRepository()
 
 class ProductService:
 
+    
     def save(self, product: Product) -> Product:
         #Save new product
         saved_product = repository.save(product)
         cache.set(f'product_{saved_product.id}', saved_product, timeout=120)
         return saved_product
 
+    @retry(wait=wait_fixed(5), stop=stop_after_attempt(5))
     def all(self) -> List[Product]:
         #Return all products
-        products = cache.get('all_products')
-        if not products:
-            products = repository.all()
-            if products: cache.set('all_products', products, timeout=120) 
-        return products
+        try:
+            products = cache.get('all_products')
+            if not products:
+                products = repository.all()
+                if products: cache.set('all_products', products, timeout=120) 
+            return products
+        except OperationalError as e:
+            logging.error(f"Error connecting database: {str(e)}")
+            raise 
 
+    @retry(wait=wait_fixed(5), stop=stop_after_attempt(5))
     def find(self, id: int) -> Optional[Product]:
         #Return product by id
-        product = cache.get(f'product_{id}')
-        if product is None:
-            product = repository.find(id)
-            if product: cache.set(f'product_{id}', product, timeout=120)
-        return product
+        try:
+            product = cache.get(f'product_{id}')
+            if product is None:
+                product = repository.find(id)
+                if product: cache.set(f'product_{id}', product, timeout=120)
+            return product
+        except OperationalError as e:
+            logging.error(f"Error connecting database: {str(e)}")
+            raise 
 
     # def save(self, product: Product) -> Product:
     #     #Save new product
