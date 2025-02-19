3a00937e73c4852064e3d100ec7f1fc9df8dbcab
LautaroPerezCarullo <laupercar1100@gmail.com>
2025-02-19 15:00:00 -0300

    Modify gunicorn config & Add basic logs to MS

--- payment_service/app/repository.py
+++ payment_service/app/repository.py
@@ -1,5 +1,6 @@
 from .model import Payment
 from app import db
+import logging
 
 class PaymentRepository:
     
@@ -7,10 +8,12 @@ class PaymentRepository:
         # Save new payment
         db.session.add(payment)
         db.session.commit()
+        logging.info(f"Successful Payment")
         return payment
     
     def delete(self, payment_id: int) -> Payment:
         payment = Payment.query.get(payment_id)
         payment.deleted_at = db.func.now()
         db.session.commit()
+        logging.info(f"Successful Payment Compensation")
         return payment
--- purchase_service/app/repository.py
+++ purchase_service/app/repository.py
@@ -1,5 +1,6 @@
 from .model import Purchase
 from app import db
+import logging
 
 class PurchaseRepository:
     
@@ -8,11 +9,13 @@ class PurchaseRepository:
         # Save new purchase
         db.session.add(purchase)
         db.session.commit()
+        logging.info(f"Successful Purchase")
         return purchase
     
     def delete(self, purchase_id: int) -> Purchase:
         purchase = Purchase.query.get(purchase_id)
         purchase.deleted_at = db.func.now()
         db.session.commit()
+        logging.info(f"Successful Purchase Compensation")
         return purchase
     
